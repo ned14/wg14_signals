ac648652b4c54b3227699f64d0ebe0c9a622799d
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2024-11-22 15:00:57 +0100

    First commit with implementations of:
    - `async_signal_safe_tls`
    - `current_thread_id`

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Reference implementation for proposed extensions to standard C signals handling
+
+(C) 2024 Niall Douglas [http://www.nedproductions.biz/](http://www.nedproductions.biz/)
+
+## Example of use
+
+```c
+todo
+```
---
+++ include/wg14_signals/config.h
@@ -0,0 +1,49 @@
+/* Proposed WG14 improved signals support
+(C) 2024 Niall Douglas <http://www.nedproductions.biz/>
+File Created: Nov 2024
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License in the accompanying file
+Licence.txt or at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#ifndef WG14_SIGNALS_CONFIG_H
+#define WG14_SIGNALS_CONFIG_H
+
+#ifndef WG14_SIGNALS_PREFIX
+#define WG14_SIGNALS_PREFIX(x) x
+#endif
+
+#ifndef WG14_SIGNALS_INLINE
+#define WG14_SIGNALS_INLINE inline
+#endif
+
+#ifndef WG14_SIGNALS_THREAD_LOCAL
+#define WG14_SIGNALS_THREAD_LOCAL thread_local
+#endif
+
+#ifndef WG14_SIGNALS_EXTERN
+#define WG14_SIGNALS_EXTERN extern
+#endif
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
